57cbba50c05948a4b1c643b707fbab97d97d74bf
Debangshu <kanthadebangshu97@gmail.com>
2022-10-02 21:56:36 +0530

    added a string

--- sum.java
+++ sum.java
@@ -2,5 +2,6 @@ public class sum{
     public static void main(String args[]){
         int a =5,b=10;
         System.out.println("hello "+(a*b));
+        System.out.println("and this is a commit");
     }
 }
