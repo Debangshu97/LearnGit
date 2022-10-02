b74dc3313dc840809c9504eb51993ee530a08155
Debangshu <kanthadebangshu97@gmail.com>
2022-10-02 20:10:43 +0530

    multiplication time

--- sum.java
+++ sum.java
@@ -1,6 +1,6 @@
 public class sum{
     public static void main(String args[]){
         int a =5,b=10;
-        System.out.println("hello "+a+b);
+        System.out.println("hello "+(a*b));
     }
 }
